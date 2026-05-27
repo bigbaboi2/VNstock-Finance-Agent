c1117bc33968233db1e0f571bcab66a785f9cf87
bigbaboi2 <99955290+bigbaboi2@users.noreply.github.com>
2026-05-27 12:11:08 +0700

    set trust proxy for improved request handling

--- src/server.js
+++ src/server.js
@@ -27,6 +27,8 @@ import { startCronJobs } from './jobs/newsCron.js';
 const app = express();
 const PORT = 3001;
 
+app.set('trust proxy', 1);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, '../.env') });
